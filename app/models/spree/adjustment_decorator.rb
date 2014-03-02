a874eada200adea13d9f7dfb812bab09fef68cbd
Bryan Mahoney <bryan@goDynamo.com>
2014-03-02 11:28:22 -0500

    Allows zero dollar promotion to be applied

---
+++ app/models/spree/adjustment_decorator.rb
@@ -0,0 +1,10 @@
+Spree::Adjustment.class_eval do
+  def set_eligibility_with_zero_fix
+    if originator.calculator.class == Spree::Calculator::FreeShippingSelection
+      update_attribute_without_callbacks(:eligible, true)
+    else
+      set_eligibility_without_zero_fix
+    end
+  end
+  alias_method_chain :set_eligibility, :zero_fix
+end
