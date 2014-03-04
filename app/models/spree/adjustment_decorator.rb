15c8c88482943ea124eb8ef85389cae68985a4c5
Bryan Mahoney <bryan@goDynamo.com>
2014-03-03 20:11:40 -0500

    Ensures an originator is present

--- app/models/spree/adjustment_decorator.rb
+++ app/models/spree/adjustment_decorator.rb
@@ -1,6 +1,6 @@
 Spree::Adjustment.class_eval do
   def set_eligibility_with_zero_fix
-    if originator.calculator.class == Spree::Calculator::FreeShippingSelection
+    if originator && originator.calculator.class == Spree::Calculator::FreeShippingSelection
       update_attribute_without_callbacks(:eligible, true)
     else
       set_eligibility_without_zero_fix
